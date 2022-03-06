396a0a90d6c2d47417abd36c170ef8f160cc21c2
LucaErr <73197383+LucaErr@users.noreply.github.com>
2022-03-06 01:43:01 +0100

    update

---
+++ config/polybar/hide.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+/home/luca/.config/hideIt/./hideIt.sh --name 'polybar-bottom_eDP-1' --hover
---
+++ config/polybar/kill.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
